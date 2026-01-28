0e94a5a49e29c2b0e72dd9580c5f991e0fe9e0c2
DyuzhovAV <artemduzhov200105@mail.ru>
2026-01-28 10:33:17 +0300

    Коммиты по изменениям

--- backand/app/main.py
+++ backand/app/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+CURRENT_TRACK_ID = "12345"
+
+class TrackUpdate(BaseModel):
+    track_id : str
+
+@app.post("/api/current-track")
+async def update_track(data:TrackUpdate):
+    global CURRENT_TRACK_ID
+    CURRENT_TRACK_ID = data.track_id
+    return {"status": "ok", "track_id": CURRENT_TRACK_ID}
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {"request": request,
+         "track_id" : CURRENT_TRACK_ID
+        }
     )
---
+++ backand/app/static/js/player.js
@@ -0,0 +1,4 @@
+function playLocalTrack(tracID) {
+    const iframe = document.getElementById("player"):
+    iframe.src = "https://music.yandex.ru/iframe/#track/{{track_id}}":
+}
